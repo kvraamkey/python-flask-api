5b52d5655dc24493c4e7b9a605bb184f9bb806e5
ramki <kvraamkeydev@gmail.com>
2020-08-07 00:49:27 +0530

    chore: api setup

--- app/resources/response.py
+++ app/resources/response.py
@@ -12,24 +12,10 @@ def successResponse(message, data):
         "message": str(message)
     }
     if (data):
-        response.update({"count": len(data),"data": data })
+        response.update({
+            "count": len(data),
+            "data": data
+        })
+    
     return jsonify(response)
 
-#404
-@response.app_errorhandler(404)
-def handle_404(err):
-    return errorResponse(err)
-
-#500
-@response.app_errorhandler(500)
-def handle_500(err):
-    return jsonify({
-        "error" : true,
-        "message": "Something went wrong",
-        "status": 500
-    })
-
-#405
-@response.app_errorhandler(405)
-def handle_500(err):
-    return errorResponse(err)

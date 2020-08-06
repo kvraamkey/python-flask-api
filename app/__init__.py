f0ca0381c9c1a627bda6f520da21a07b8dbc34c7
ramki <ramki.ayyamperumal@everestdx.com>
2020-08-07 02:05:40 +0530

    feat: api setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
-from .database.db import initialize_es;
-from .resources.routes import initialize_routes
-from .resources.response import errorResponse
+from app.database import initialize_es
+from app.routes import initialize_routes
+from app.utils import errorResponse
 
 #Define the WSGI application object
 app = Flask(__name__)
@@ -11,6 +11,7 @@ def create_app():
     clientEs = initialize_es(app)
     
     #Exception Error Handling
+    app.register_error_handler(405, errorResponse)
     app.register_error_handler(404, errorResponse)
     app.register_error_handler(500, errorResponse)
 
--- app/resources/auth.py
+++ app/resources/auth.py
@@ -1,12 +1,24 @@
-from flask import current_app, Blueprint,request, jsonify
-from .response import successResponse, errorResponse
+from flask import current_app, Blueprint, request, jsonify
+from elasticsearch import exceptions
+from app.utils import successResponse, errorResponse
 auth = Blueprint('auth', __name__)
 
 @auth.route('/signin', methods=['POST'])
 def LoginApi():
-    # es = current_app.config["es"]
+    
+    elastic_client = current_app.config["es"]
+
     try:
-        return successResponse("test mesasage",request)
+        query_body = {
+            "query": {
+                "match_all": {}
+            },
+            "size": 10,
+        }
+        result = elastic_client.search(index="o365_azure_ad_users", body=query_body)
+        return successResponse("test mesasage", result)
+    except exceptions.ConnectionError as err:
+        return errorResponse(err)
     except Exception as e:
         return errorResponse(e)
 

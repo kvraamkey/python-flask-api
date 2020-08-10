55c7768e964b2bddc3e823d51562b0e084ff45d7
ramki <kvraamkeydev@gmail.com>
2020-08-10 17:21:10 +0530

    dynamic routing enabled

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.database import initialize_es
 from app.routes import initialize_routes
-from app.utils import errorResponse
+from app.utils import error_response
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -12,9 +12,9 @@ def create_app():
     client_es = initialize_es()
     
     # Exception Error Handling
-    app.register_error_handler(405, errorResponse)
-    app.register_error_handler(404, errorResponse)
-    app.register_error_handler(500, errorResponse)
+    app.register_error_handler(405, error_response)
+    app.register_error_handler(404, error_response)
+    app.register_error_handler(500, error_response)
 
     # app configuration
     app.config['JSON_SORT_KEYS'] = False
--- app/resources/auth.py
+++ app/resources/auth.py
@@ -1,6 +1,6 @@
 from flask import current_app, Blueprint
 from elasticsearch import exceptions
-from app.utils import successResponse, errorResponse
+from app.utils import error_response, success_response
 auth = Blueprint('auth', __name__)
 
 
@@ -8,20 +8,20 @@ auth = Blueprint('auth', __name__)
 def login():
     elastic_client = current_app.config["es"]
     try:
-        # query_body = {
-        #     "query": {
-        #         "match_all": {}
-        #     },
-        #     "size": 10,
-        # }
-        # result = elastic_client.search(index="o365_azure_ad_users", body=query_body)
-        return successResponse("test mesasage", 'result')
+        query_body = {
+            "query": {
+                "match_all": {}
+            },
+            "size": 10,
+        }
+        result = elastic_client.search(index="cimlogs_dev", body=query_body)
+        return success_response("test mesasage", "result")
     except exceptions.ConnectionError as err:
-        return errorResponse(err)
+        return error_response(err)
     except Exception as e:
-        return errorResponse(e)
+        return error_response(e)
 
 
 @auth.route('/signup')
 def signup():
-    return successResponse("signup response")
+    return error_response("signup response")

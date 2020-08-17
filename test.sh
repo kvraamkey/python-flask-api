9bd793cedc139004a877d609f52664a5e1cd8df5
ramki <kvraamkeydev@gmail.com>
2020-08-17 23:33:33 +0530

    code cleanup

--- test.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='ramki'
-    GIT_AUTHOR_EMAIL='kvraamkeydev@gmail.com'
-    GIT_COMMITTER_NAME='ramki'
-    GIT_COMMITTER_EMAIL='kvraamkeydev@gmail.com'
-  " HEAD
